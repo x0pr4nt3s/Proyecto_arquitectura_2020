348670ef08767ed6e464b0ba356823ecc1f90eec
cristhoperHL <cristhopermma@gmail.com>
2020-06-25 14:14:17 -0500

    mips end

--- controller.v
+++ controller.v
@@ -1,13 +1,23 @@
 `ifndef _controller_v_
-`define _controller_v_ 
-module controller(op,funct,zero,memtoreg,memwrite,pcsrc,alusrc,
-                  regdst,regwrite,jump,alucontrol);
-input [5:0] op;
-input funct,zero;
+`define _controller_v_
+`include "aludec.v"
+`include "maindec.v" 
+module controller(op,funct,zero,memtoreg,memwrite,pcsrc,
+            alusrc,regdst,regwrite,jump,alucontrol);
+input [5:0] op,funct;
+input zero;
 output memtoreg,memwrite,pcsrc,alusrc,regdst,regwrite,jump;
 output [2:0] alucontrol;
 
+wire [1:0] aluop;
+wire branch;
 
+maindec md(op, memtoreg, memwrite, branch,
+             alusrc, regdst, regwrite, jump,
+             aluop);
+aludec  ad(funct, aluop, alucontrol);
+
+assign pcsrc = branch & zero;
 
 
 endmodule
--- mips.v
+++ mips.v
@@ -1,4 +1,5 @@
 `include "datapath.v"
+`include "controller.v"
 module mips(clk,reset,pc,instr,memwrite,aluout,writedata,
            readdata);
 input clk,reset;
@@ -7,7 +8,22 @@ input [31:0] instr;
 output memwrite;
 output [31:0] aluout,writedata;
 input [31:0] readdata;
+
+wire        memtoreg, branch,
+            pcsrc, zero,
+            alusrc, regdst, regwrite, jump;
+wire [2:0]  alucontrol;
     
+
+  controller c(instr[31:26], instr[5:0], zero,
+               memtoreg, memwrite, pcsrc,
+               alusrc, regdst, regwrite, jump,
+               alucontrol);
+  datapath dp(clk, reset, memtoreg, pcsrc,
+              alusrc, regdst, regwrite, jump,
+              alucontrol,
+              zero, pc, instr,
+              aluout, writedata, readdata);
 
 
 endmodule 
